7bb84158b2b3cbc4ad62e0ac84382edbe2e0d71a
Рената Цікалова <ts.renata98@gmail.com>
2022-01-12 22:23:22 +0200

    Add project files.

---
+++ ITEA_HWList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ITEA_HWList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
